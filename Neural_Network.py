baf69890295a93409bac66297e924aac555fa8ec
sakthianand7 <sakthipraba1423@gmail.com>
2020-07-14 16:44:33 +0530

    Added two functions create_model and split_data for better understanding

--- Neural_Network.py
+++ Neural_Network.py
@@ -10,14 +10,23 @@ from keras.layers import Dense
 dataset=pd.read_csv('Connect_Mobile__Attrition_Data_file.csv')
 x=dataset.iloc[:,2:].values
 y=dataset.iloc[:,0].values
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3)
-sc=StandardScaler()
-x_train=sc.fit_transform(x_train)
-x_test=sc.transform(x_test)
-classifier=Sequential()
-classifier.add(Dense(input_dim=8,output_dim=5,activation='relu',init='uniform'))
-classifier.add(Dense(output_dim=1,activation='sigmoid'))
-classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+def split_data(x,y):
+	x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3)
+	sc=StandardScaler()
+	x_train=sc.fit_transform(x_train)
+	x_test=sc.transform(x_test)
+	return x_train,x_test,y_train,y_test
+
+def create_model(input_dim):
+	classifier=Sequential()
+	classifier.add(Dense(input_dim=input_dim,output_dim=5,activation='relu',init='uniform'))
+	classifier.add(Dense(output_dim=1,activation='sigmoid'))
+	classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+	return classifier
+
+x_train,x_test,y_train,y_test = split_data(x,y)
+classifier=create_model(8)
 classifier.fit(x_train,y_train,batch_size=10,epochs=10)
 ypred=classifier.predict(x_test)
 ypred=ypred>0.5
